46cb85699906873a76db4ffcadcaa225ae866d8e
RENYwei <a109222018@mail.shu.edu.tw>
2024-04-26 13:57:09 +0800

    2024/0425

--- RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
+++ RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
@@ -1,3 +1,4 @@
+//A109222018
 package com.example.raddiobutton1;
 
 
@@ -6,47 +7,102 @@ import com.example.raddiobutton1.R;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+    private String outputStr = "";
+    private String gender = "";
+    private String type = "";
+    private int numberOfTickets = 0;
+    private int price = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        RadioGroup rgGender = (RadioGroup) findViewById(R.id.rgGender);
+        RadioGroup rgType = (RadioGroup) findViewById(R.id.rgType);
+        EditText edtNumberOfTickets = (EditText) findViewById(R.id.edtNumberOfTickets);
+        Button btnConfirm = (Button) findViewById(R.id.btnConfirm);
+
+        gender = getResources().getString(R.string.male);
+        type = getResources().getString(R.string.regularticket);
+
+        rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdbBoy) {
+                    gender = getResources().getString(R.string.male);
+                }
+                else {
+                    gender = getResources().getString(R.string.female);
+                }
+                showResult();
+            }
+        });
+
+        rgType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdbAdult) {
+                    type = getResources().getString(R.string.regularticket);
+                    price = 500;
+                }
+                else if (checkedId == R.id.rdbChild) {
+                    type = getResources().getString(R.string.childticket);
+                    price = 250;
+                }
+                else {
+                    type = getResources().getString(R.string.studentticket);
+                    price = 400;
+                }
+                showResult();
+            }
+        });
+
+        edtNumberOfTickets.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    numberOfTickets = Integer.parseInt(s.toString());
+                } catch (NumberFormatException e) {
+                    numberOfTickets = 0;
+                }
+                showResult();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String outputStr = "";
-
-                RadioButton boy = findViewById(R.id.rdbBoy);
-                RadioButton girl = findViewById(R.id.rdbGirl);
-                if (boy.isChecked())
-                    outputStr += "男生\n";
-                else if (girl.isChecked())
-                    outputStr += "女生\n";
-
-                RadioGroup type = findViewById(R.id.rgType);
-                int checkedId = type.getCheckedRadioButtonId();
-                if (checkedId ==  R.id.rdbAdult)
-                    outputStr += "全票\n";
-                else if (checkedId ==  R.id.rdbChild)
-                    outputStr += "兒童票\n";
-                else
-                    outputStr += "學生票\n";
-
                 Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                intent.putExtra("result", outputStr);
+                intent.putExtra("outputStr", outputStr);
                 startActivity(intent);
             }
         });
+    }
+
+    public void showResult() {
+        outputStr = String.format("%s\n%s%d張\n共 %d元", gender, type, numberOfTickets,
+                price * numberOfTickets);
+        ((TextView) findViewById(R.id.lblOutput)).setText(outputStr);
     }
 }
--- RaddioButton1/app/src/main/java/com/example/raddiobutton1/ResultActivity.java
+++ RaddioButton1/app/src/main/java/com/example/raddiobutton1/ResultActivity.java
@@ -1,13 +1,10 @@
 package com.example.raddiobutton1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -16,8 +13,11 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        TextView resultTextView = findViewById(R.id.resultTextView);
-        String result = getIntent().getStringExtra("result");
-        resultTextView.setText(result);
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
+        txvShow.setTextSize(30);
+
+        Intent intent = getIntent();
+        txvShow.setText("訂單：" + "\n" + intent.getStringExtra("outputStr"));
+
     }
 }
